0e97a1de6bdbf1b0383ba325da549d8acf8f2aa0
bairamovazat <Bairamovazat@gmail.com>
2019-05-06 21:13:09 +0300

    bf

--- src/main/java/ru/ivmiit/web/config/AppConfiguration.java
+++ src/main/java/ru/ivmiit/web/config/AppConfiguration.java
@@ -1,16 +1,22 @@
 package ru.ivmiit.web.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -22,36 +28,17 @@ import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 import ru.ivmiit.web.security.config.SecurityConfig;
 
 import javax.servlet.FilterRegistration;
+import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import java.util.List;
 import java.util.Properties;
 
-
-@Configuration
-@EnableWebMvc
-@EnableAsync
-@PropertySource("classpath:application.properties")
-@Import({JpaConfiguration.class, SecurityConfig.class})
-@ComponentScan(basePackages = {"ru.ivmiit.web.model", "ru.ivmiit.web.service", "ru.ivmiit.web.controller"})
+@SpringBootApplication
+@ComponentScan("ru.ivmiit.web")
+@EnableJpaRepositories("ru.ivmiit.web.repository")
+@EntityScan(basePackages = "ru.ivmiit.web.model", basePackageClasses = Jsr310JpaConverters.class)
 public class AppConfiguration implements WebMvcConfigurer {
-
-
-    @Value("${email.name}")
-    private String emailName;
-
-    @Value("${email.password}")
-    private String emailPassword;
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new MappingJackson2HttpMessageConverter());
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -75,51 +62,16 @@ public class AppConfiguration implements WebMvcConfigurer {
     }
 
     @Bean
-    public FreeMarkerViewResolver getFmViewResolver() {
-        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
-        resolver.setCache(true);
-        resolver.setPrefix("");
-        resolver.setSuffix(".ftl");
-        resolver.setContentType("text/html; charset=UTF-8");
-        resolver.setOrder(0);
-        return resolver;
-    }
-
-    @Bean
-    public FreeMarkerConfigurer freemarkerConfig() {
-        FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
-        freeMarkerConfigurer.setTemplateLoaderPath("/WEB-INF/classes/templates/");
-        return freeMarkerConfigurer;
-    }
-
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername(emailName);
-        mailSender.setPassword(emailPassword);
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
+    public MultipartConfigElement multipartConfigElement() {
+        return new MultipartConfigElement("");
     }
 
 
-
-//    @Bean
-//    public DriverManagerDataSource driverManagerDataSource(){
-//        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-//        driverManagerDataSource.setDriverClassName(driverClassName);
-//        driverManagerDataSource.setUrl(url);
-//        driverManagerDataSource.setUsername(username);
-//        driverManagerDataSource.setPassword(password);
-//        return driverManagerDataSource;
-//    }
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(1024*1024*100);
+        return multipartResolver;
+    }
 
 }
